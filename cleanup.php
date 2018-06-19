568dc9a7e8af91e7d3c3d844cc56329f2d4454ed
kojuharov <31001170+kojuharov@users.noreply.github.com>
2018-06-19 16:54:58 +0300

    Update cleanup.php

--- cleanup.php
+++ cleanup.php
@@ -1,31 +1,26 @@
 <?php
-
 /**
  * Cleans up the environment after the test run.
  */
-
 require __DIR__ . '/functions.php';
-
 // Check required environment variables
 check_required_env();
-
 // Bring some environment variables into scope
 $WPT_PREPARE_DIR = getenv( 'WPT_PREPARE_DIR' );
 $WPT_SSH_CONNECT = getenv( 'WPT_SSH_CONNECT' );
 $WPT_SSH_OPTIONS = getenv( 'WPT_SSH_OPTIONS' ) ? : '-o StrictHostKeyChecking=no';
 $WPT_TEST_DIR = getenv( 'WPT_TEST_DIR' );
 $WPT_RM_TEST_DIR_CMD = getenv( 'WPT_RM_TEST_DIR_CMD' ) ? : 'rm -r ' . $WPT_TEST_DIR;
-
 // Clean up the preparation directory
 // Only forcefully delete the .git directory, to prevent disasters otherwise
 perform_operations( array(
 	'rm -rf ' . escapeshellarg( $WPT_PREPARE_DIR . '/.git' ),
 	'rm -r ' . escapeshellarg( $WPT_PREPARE_DIR ),
 ) );
-
 // Clean up the test directory in remote environments
-if ( false !== $WPT_SSH_CONNECT ) {
+if ( ! empty( $WPT_SSH_CONNECT ) ) {
 	perform_operations( array(
 		'ssh ' . $WPT_SSH_OPTIONS . ' ' . escapeshellarg( $WPT_SSH_CONNECT ) . ' ' . escapeshellarg( $WPT_RM_TEST_DIR_CMD ),
 	) );
 }
+
